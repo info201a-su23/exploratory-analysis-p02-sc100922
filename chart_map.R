7c503a3ec8dde92176a87823b72659b46590b192
Wendyb22 <137957393+Wendyb22@users.noreply.github.com>
2023-08-04 22:14:16 -0700

    Update chart_map.R

--- chart_map.R
+++ chart_map.R
@@ -4,10 +4,13 @@ library(maps)
 library(scales)
 library(stringr)
 
+# load data
 gunshot <- read.csv("/Users/wendybu/Desktop/info201/groupproject/exploratory-analysis-p02-sc100922/all_incidents.csv")
 
+# Get the state borders data for the US
 us_states <- map_data("state")
 
+# Get the total number of incidents per state
 total_per_state <- gunshot %>% 
   group_by(state) %>%
   summarise(total_incidents = n())
@@ -21,14 +24,13 @@ state_shape <- map_data("state")
 
 # Capitalize the first letter of each word in the region column
 state_shape$region <- str_to_title(state_shape$region)
+state_shape <- state_shape %>%
+  select(-subregion)
 
 # Combines the two data sets
 total_per_state <- total_per_state %>% 
   mutate(full_state_name = tolower(full_state_name))
-total_incidents_shape <- left_join(total_per_state, state_shape, by = c("full_state_name" = "region"))
-
-# Convert total_incidents to numeric if needed
-total_incidents_shape$total_incidents <- as.numeric(total_incidents_shape$total_incidents)
+total_incidents_shape <- left_join(total_per_state, state_shape, by = c("state" = "region"))
 
 # Plot the map with filled polygons based on total_incidents variable
 total_incidents_map <- ggplot(data = total_incidents_shape) +
